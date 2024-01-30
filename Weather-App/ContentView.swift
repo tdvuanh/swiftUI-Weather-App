0f3734b9a5a5ccd5b84b9cb03326ea919e6f0fc0
tduyvuanh <75930779+tdvuanh@users.noreply.github.com>
2024-01-30 23:08:37 +0700

    first commit with base UI

--- Weather-App/ContentView.swift
+++ Weather-App/ContentView.swift
@@ -6,56 +6,79 @@
 //
 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    @State private var isNight = false
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
+        ZStack {
+            BackgroundView(isNight: $isNight)
+            VStack {
+                CityTextView(cityName: "Your Location")
+                WeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: 35)
+                HStack(spacing: 20) {
+                    WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 38)
+                    WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 42)
+                    WeatherDayView(dayOfWeek: "THU", imageName: "cloud.sun.bolt.fill", temperature: 32)
+                    WeatherDayView(dayOfWeek: "FRI", imageName: "cloud.rain.fill", temperature: 30)
+                    WeatherDayView(dayOfWeek: "SAT", imageName: "cloud.hail.fill", temperature: 28)
                 }
-                .onDelete(perform: deleteItems)
+                Spacer()
+                
+                Button {
+                    isNight.toggle()
+                } label: {
+                    WeatherButton(title: "Change your day", textColor: .blue, backColor: .white)
+                }
+                Spacer()
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
+    
+}
+
+struct WeatherDayView: View {
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek).font(.system(size: 16, weight: .medium, design: .default)).foregroundColor(.white)
+            Image(systemName: imageName).renderingMode(.original).resizable().aspectRatio(contentMode: .fit).frame(width: 40, height: 40)
+            Text("\(temperature)").font(.system(size: 28, weight: .medium)).foregroundColor(.white)
         }
     }
+}
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
+struct BackgroundView: View {
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : .purple]), startPoint: .topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName).font(.system(size: 32, weight: .medium, design: .default)).foregroundColor(.white)
+    }
+}
+
+struct WeatherStatusView: View {
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Image(systemName: imageName).renderingMode(.original).resizable().aspectRatio(contentMode: .fit).frame(width: 180, height: 180)
+            Text("\(temperature)").font(.system(size: 70, weight: .medium)).foregroundColor(.white)
+        }.padding(.bottom, 40)
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
--- Weather-App/WeatherButton.swift
+++ Weather-App/WeatherButton.swift
@@ -5,4 +5,14 @@
 //  Created by Trần Duy Vũ Anh on 30/01/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct WeatherButton: View {
+    var title: String
+    var textColor: Color
+    var backColor: Color
+    
+    var body: some View {
+        Text(title).frame(width: 280, height: 50).background(backColor).foregroundColor(textColor).font(.system(size: 20, weight: .bold, design: .default)).cornerRadius(10)
+    }
+}
